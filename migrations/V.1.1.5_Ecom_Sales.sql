b1f4e182b19bce26061bbdd939c9ed1978b2847b
irynaprykhodko <irina.prykhodko@gmail.com>
2022-12-13 12:47:06 +0100

    new file:   migrations/V.1.1.5_Ecom_Sales.sql

---
+++ migrations/V.1.1.5_Ecom_Sales.sql
@@ -0,0 +1,9 @@
+create or replace file format csv_format
+  type = csv
+  field_delimiter = '|'
+  skip_header = 1
+  FIELD_OPTIONALLY_ENCLOSED_BY = '"';
+
+create or replace stage stg_article_masterdata file_format = csv_format;
+create or replace stage stg_ecom_sales file_format = csv_format;
+create or replace stage stg_calendar file_format = csv_format;
